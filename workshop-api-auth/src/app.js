dd6f3815d25d1c2732e72e51771280402ed9d144
up1 <somkiat.p@gmail.com>
2020-06-19 00:56:04 +0700

    Add page not foundd

--- workshop-api-auth/src/app.js
+++ workshop-api-auth/src/app.js
@@ -4,6 +4,14 @@ const { notFoundHandler, productionErrorHandler } = require("./middleware/");
 
 const server = express();
 
+// Page not found
+server.use((req, res, next) => {
+  res.status(404).send({
+    status: 404,
+    error: "Not found",
+  });
+});
+
 server.use(express.json());
 server.use(homeRoute);
 
