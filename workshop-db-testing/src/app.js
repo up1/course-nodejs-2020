a4ffb2da67a55a6f7c652cdcfdb365ef886df264
up1 <somkiat.p@gmail.com>
2020-06-18 22:07:12 +0700

    Working with aauthen

--- workshop-db-testing/src/app.js
+++ workshop-db-testing/src/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const authorization = require("./auth");
+
 const app = express();
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ _id: "1" }, process.env.JWT_SECRET);
+  res.send(token);
+});
+app.get("/secure", authorization, (req, res) => res.send("Secure ..."));
 
 const productService = require("./services/product-service");
 app.get("/test", async (req, res) => {
---
+++ workshop-db-testing/src/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // const id = decoded._id;
+    const user = { id: 1, name: "fake user" };
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send({ error: "Please authenticate." });
+  }
+};
+
+module.exports = auth;
